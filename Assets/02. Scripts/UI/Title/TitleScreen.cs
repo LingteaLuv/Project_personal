f5251df8017428122e548860f9e2c7ea6b242f88
01stone01 <hagwhr2@gmail.com>
2025-06-03 18:44:34 +0900

    [Feat] Add title Scene
    #75 타이틀 씬 구현
    게임 시작, 게임 종료 기능 추가

---
+++ Assets/02. Scripts/UI/Title/TitleScreen.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    [Header("Drag&Drop")] 
+    [SerializeField] private Button _startBtn;
+    [SerializeField] private Button _settingBtn;
+    [SerializeField] private Button _exitBtn;
+    
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Start()
+    {
+        _startBtn.onClick.AddListener(() => SceneManager.LoadScene("InGame", LoadSceneMode.Single));
+        _startBtn.onClick.AddListener(() => SceneManager.LoadScene("UI", LoadSceneMode.Additive));
+        _settingBtn.onClick.AddListener(() => SettingManager.Instance.EnterSettingUI());
+        _exitBtn.onClick.AddListener(() => 
+        {
+            Application.Quit();
+            Debug.Log("게임 종료");
+        });
+    }
+
+    private void Init()
+    {
+        
+    }
+}
