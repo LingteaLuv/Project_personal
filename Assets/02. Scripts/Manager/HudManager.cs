c2de5deaf4863b621212cca2b3b5a8967ec6fe72
01stone01 <hagwhr2@gmail.com>
2025-06-04 14:47:18 +0900

    [Feat] Add property value in ingame hud
    #90 인게임 hud에 체력, 정신력, 허기 값 표시 기능 추가

--- Assets/02. Scripts/Manager/HudManager.cs
+++ Assets/02. Scripts/Manager/HudManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,20 +10,27 @@ public class HudManager : Singleton<HudManager>
     [SerializeField] private Image _hpImage;
     [SerializeField] private Image _mentalityImage;
     [SerializeField] private Image _hungerImage;
+    [SerializeField] private TMP_Text _hpText;
+    [SerializeField] private TMP_Text _mentalityText;
+    [SerializeField] private TMP_Text _hungerText;
+    
     
     private void UpdateHpUI(int curHp)
     {
         _hpImage.fillAmount = (float)curHp / 100;
+        _hpText.text = $"{curHp}";
     }
 
     private void UpdateMentalityUI(float curMentality)
     {
         _mentalityImage.fillAmount = curMentality / 100;
+        _mentalityText.text = $"{(int)curMentality}";
     }
     
     private void UpdateHungerUI(float curHunger)
     {
         _hungerImage.fillAmount = curHunger / 100;
+        _hungerText.text = $"{(int)curHunger}";
     }
 
     public void Subscribe(PlayerProperty playerProperty)
